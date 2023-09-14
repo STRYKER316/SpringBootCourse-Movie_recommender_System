60fd9955385b0c6b98d50c00aa1d8c2007dd259c
Stryker316 <ansil.mishra316@gmail.com>
2023-09-14 17:48:03 +0530

    Recommender class, Filter class implemented

---
+++ src/main/java/com/example/springbasics/movierecommendersystem/lesson1/MovieRecommenderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbasics.movierecommendersystem.lesson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springbasics/movierecommendersystem/lesson1/RecommenderImplementation.java
@@ -0,0 +1,9 @@
+package com.example.springbasics.movierecommendersystem.lesson1;
+
+public class RecommenderImplementation {
+    public String[] recommendMovies (String movie) {
+        //use content based filter to find similar movies
+        //return the results
+        return new String[] {"M1", "M2", "M3"};
+    }
+}
