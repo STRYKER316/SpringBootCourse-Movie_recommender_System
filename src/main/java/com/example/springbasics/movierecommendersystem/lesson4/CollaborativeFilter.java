7a6523c01a2a83d3a7b3d6388ca41993aa87a5fc
Stryker316 <ansil.mishra316@gmail.com>
2023-09-14 20:29:03 +0530

    lesson4 sub-package created, same intial config as lesson3

---
+++ src/main/java/com/example/springbasics/movierecommendersystem/lesson4/CollaborativeFilter.java
@@ -0,0 +1,10 @@
+package com.example.springbasics.movierecommendersystem.lesson4;
+
+public class CollaborativeFilter implements Filter {
+    public String[] getRecommendations(String movie) {
+        // implement logic of collaborative filter
+
+        // return movie recommendations
+        return new String[] { };
+    }
+}
---
+++ src/main/java/com/example/springbasics/movierecommendersystem/lesson4/MovieRecommenderSystemApplication.java
@@ -0,0 +1,24 @@
+package com.example.springbasics.movierecommendersystem.lesson4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class MovieRecommenderSystemApplication {
+    public static void main(String[] args) {
+        // Application manages the beans and dependencies
+        ApplicationContext appContext = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+
+        // Use ApplicationContext to find which filter is being used
+        RecommenderImplementation movieRecommender = appContext.getBean(RecommenderImplementation.class);
+
+        // call method to get recommendations
+        String[] recommendedMovies = movieRecommender.recommendMovies("Finding Dory");
+
+        // display results
+        System.out.println(Arrays.toString(recommendedMovies));
+    }
+}
