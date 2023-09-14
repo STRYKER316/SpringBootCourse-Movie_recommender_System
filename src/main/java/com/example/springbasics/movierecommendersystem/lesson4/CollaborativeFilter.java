4701b12d354ebbf5e21060774dcf4c97ad5eb625
Stryker316 <ansil.mishra316@gmail.com>
2023-09-14 20:33:17 +0530

    CollaborativeFilter class is made the @Primary bean for Recommendation class

--- src/main/java/com/example/springbasics/movierecommendersystem/lesson4/CollaborativeFilter.java
+++ src/main/java/com/example/springbasics/movierecommendersystem/lesson4/CollaborativeFilter.java
@@ -1,10 +1,15 @@
 package com.example.springbasics.movierecommendersystem.lesson4;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class CollaborativeFilter implements Filter {
     public String[] getRecommendations(String movie) {
         // implement logic of collaborative filter
 
         // return movie recommendations
-        return new String[] { };
+        return new String[] {"Finding Nemo", "Ice Age", "Toy Story"};
     }
 }
