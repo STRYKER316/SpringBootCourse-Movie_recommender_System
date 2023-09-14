b960cbbc839eb395796f0a971b25c7a3e160de23
Stryker316 <ansil.mishra316@gmail.com>
2023-09-14 17:50:10 +0530

    ContentBasedFilter dependency used in Recommender class

--- src/main/java/com/example/springbasics/movierecommendersystem/lesson1/RecommenderImplementation.java
+++ src/main/java/com/example/springbasics/movierecommendersystem/lesson1/RecommenderImplementation.java
@@ -3,7 +3,10 @@ package com.example.springbasics.movierecommendersystem.lesson1;
 public class RecommenderImplementation {
     public String[] recommendMovies (String movie) {
         //use content based filter to find similar movies
+        ContentBasedFilter filter = new ContentBasedFilter();
+        String[] recommendedMovies = filter.getRecommendations(movie);
+
         //return the results
-        return new String[] {"M1", "M2", "M3"};
+        return recommendedMovies;
     }
 }
