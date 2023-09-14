847e4ee7746a1e147785444193d3cf085d3558e7
Stryker316 <ansil.mishra316@gmail.com>
2023-09-14 18:13:10 +0530

    Collaborative Filter class created

---
+++ src/main/java/com/example/springbasics/movierecommendersystem/lesson2/CollaborativeFilter.java
@@ -0,0 +1,8 @@
+package com.example.springbasics.movierecommendersystem.lesson2;
+
+public class CollaborativeFilter {
+    public String[] getRecommendations(String movie) {
+        //logic of collaborative filter
+        return new String[] { };
+    }
+}
